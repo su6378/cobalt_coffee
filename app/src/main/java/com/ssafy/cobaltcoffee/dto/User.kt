16684f9b6eaf9f3bea3e90ed51d1bc4bdede8dc5
rigizer <rigizer@gmail.com>
2022-11-19 10:50:44 +0900

    UserDTO 위치정보, 마케팅동의, 광고성알림여부 인스턴스 변수 추가

--- app/src/main/java/com/ssafy/cobaltcoffee/dto/User.kt
+++ app/src/main/java/com/ssafy/cobaltcoffee/dto/User.kt
@@ -2,7 +2,16 @@ package com.ssafy.cobaltcoffee.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class User(val id: String,  var name: String, @SerializedName("pass") var pw: String, val stamps: Int, val stampList: ArrayList<Stamp> = ArrayList()) : java.io.Serializable
+data class User(
+    val id: String,
+    var name: String,
+    @SerializedName("pass") var pw: String,
+    val stamps: Int,
+    val stampList: ArrayList<Stamp> = ArrayList(),
+    val isPush: Boolean = true,
+    val isLocation: Boolean = true,
+    val isMarketing: Boolean = true
+) : java.io.Serializable
 {
     constructor() : this("","","",0)
     constructor(id:String, pw:String) : this(id,"",pw,0)
