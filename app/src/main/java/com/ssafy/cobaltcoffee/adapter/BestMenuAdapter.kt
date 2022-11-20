a8f88feadc6889fdb1426806112637a0dd79dbd0
su6378 <josooyeon11@naver.com>
2022-11-20 21:02:45 +0900

    HomeFragment UI 추가 및 수정

---
+++ app/src/main/java/com/ssafy/cobaltcoffee/adapter/BestMenuAdapter.kt
@@ -0,0 +1,60 @@
+package com.ssafy.cobaltcoffee.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ssafy.cobaltcoffee.R
+import com.ssafy.cobaltcoffee.config.ApplicationClass
+import com.ssafy.cobaltcoffee.dto.Product
+
+private const val TAG = "MenuAdapter_코발트"
+class BestMenuAdapter(var productList:List<Product>) :RecyclerView.Adapter<BestMenuAdapter.BestMenuHolder>(){
+
+    inner class BestMenuHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val menuName = itemView.findViewById<TextView>(R.id.bm_name)
+        val menuImage = itemView.findViewById<ImageView>(R.id.bm_img)
+
+        fun bindInfo(product : Product){
+            menuName.text = product.name
+            Glide.with(itemView)
+                .load("${ApplicationClass.MENU_IMGS_URL}${product.image}")
+                .into(menuImage)
+
+            itemView.setOnClickListener{
+                itemClickListner.onClick(it, layoutPosition, productList[layoutPosition].id)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BestMenuHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_best_menu, parent, false)
+        return BestMenuHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BestMenuHolder, position: Int) {
+        holder.apply{
+            bindInfo(productList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+    //클릭 인터페이스 정의 사용하는 곳에서 만들어준다.
+    interface ItemClickListener {
+        fun onClick(view: View,  position: Int, productId:Int)
+    }
+
+    //클릭리스너 선언
+    private lateinit var itemClickListner: ItemClickListener
+    //클릭리스너 등록 매소드
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListner = itemClickListener
+    }
+}
+
--- app/src/main/java/com/ssafy/cobaltcoffee/home/HomeFragment.kt
+++ app/src/main/java/com/ssafy/cobaltcoffee/home/HomeFragment.kt
@@ -1,20 +1,38 @@
 package com.ssafy.cobaltcoffee.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.ssafy.cobaltcoffee.R
+import com.ssafy.cobaltcoffee.adapter.BestMenuAdapter
 import com.ssafy.cobaltcoffee.databinding.FragmentHomeBinding
+import com.ssafy.cobaltcoffee.dto.Product
+import com.ssafy.cobaltcoffee.home.order.ProductActivity
+import com.ssafy.cobaltcoffee.home.order.ProductAdapter
+import com.ssafy.cobaltcoffee.repository.ProductRepository
+import com.ssafy.smartstore.util.RetrofitCallback
 
+private const val TAG = "HomeFragment_코발트"
 class HomeFragment : Fragment() {
     private lateinit var homeActivity: HomeActivity
     private lateinit var binding: FragmentHomeBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var bestMenuList: MutableList<Product> = mutableListOf()
+    private lateinit var bestMenuAdapter: BestMenuAdapter
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        homeActivity = context as HomeActivity
     }
 
     override fun onCreateView(
@@ -32,14 +50,57 @@ class HomeFragment : Fragment() {
         binding.homeBanner.setOnClickListener {
 
         }
+
+
     }
 
     private fun init() {
 
-        Glide.with(requireContext()).load(R.drawable.background_home).into(binding.homeImg)
-        Glide.with(requireContext()).load(R.drawable.banner).into(binding.homeBanner)
+        getBestMenu()
 
+        //이미지 적용 (Glide 라이브러리 사용)
+        val options = RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE)
 
+        Glide.with(requireContext()).load(R.drawable.background_home).apply(options).into(binding.homeImg) //툴바 쪽 이미지
+        Glide.with(requireContext()).load(R.drawable.banner).apply(options).into(binding.homeBanner) //신상품 이미지
+        Glide.with(this).load(R.drawable.banner3).apply(options).into(binding.homeBanner2) //신상메뉴 이미지
 
+        binding.apply {
+            bestMenuAdapter = BestMenuAdapter(bestMenuList)
+            bestMenuAdapter.setItemClickListener(object : BestMenuAdapter.ItemClickListener {
+                override fun onClick(view: View, position: Int, productId: Int) {
+
+                }
+            })
+
+            homeRv.apply {
+                layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                adapter = bestMenuAdapter
+                //원래의 목록위치로 돌아오게함
+                adapter!!.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            }
+        }
+
+    }
+
+    //BestMenu 가져오기
+    private fun getBestMenu(){
+        ProductRepository.get().getBestProductList(MenuListCallback())
+    }
+
+    inner class MenuListCallback: RetrofitCallback<List<Product>> {
+        override fun onSuccess(code: Int, result: List<Product>) {
+            bestMenuList = result as MutableList<Product>
+            bestMenuAdapter.productList = bestMenuList
+            bestMenuAdapter.notifyDataSetChanged()
+        }
+
+        override fun onError(t: Throwable) {
+            Log.d(TAG, t.message ?: "메뉴 정보 불러오는 중 통신오류")
+        }
+
+        override fun onFailure(code: Int) {
+            Log.d(TAG, "onResponse: Error Code $code")
+        }
     }
 }
