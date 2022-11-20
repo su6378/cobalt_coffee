66c16743b1593ad29373e5f9283aef395df6a47a
rigizer <rigizer@gmail.com>
2022-11-20 12:43:15 +0900

    MenuFragment: RecyclerView로 변경, 이미지 서버에서 받아올 수 있도록 Glide 적용

--- app/src/main/java/com/ssafy/cobaltcoffee/home/order/MenuFragment.kt
+++ app/src/main/java/com/ssafy/cobaltcoffee/home/order/MenuFragment.kt
@@ -1,14 +1,22 @@
 package com.ssafy.cobaltcoffee.home.order
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.view.menu.MenuAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.cobaltcoffee.config.ApplicationClass
 import com.ssafy.cobaltcoffee.databinding.FragmentMenuBinding
 import com.ssafy.cobaltcoffee.dto.Product
 import com.ssafy.cobaltcoffee.home.HomeActivity
+import okhttp3.internal.immutableListOf
 
+private const val TAG = "MenuFragment_코발트"
 class MenuFragment(productType: Int) : Fragment() {
     private lateinit var homeActivity: HomeActivity
     private lateinit var menuFragment: MenuFragment
@@ -32,27 +40,35 @@ class MenuFragment(productType: Int) : Fragment() {
     }
 
     private fun init() {
-        val productList: ArrayList<Product> = arrayListOf()
-        val productTypes: ArrayList<String> = arrayListOf("과자", "음료", "차")
+        val productList: MutableList<Product> = mutableListOf<Product>()
+        val productTypes: List<String> = mutableListOf<String>("과자", "음료", "차")
         productList.apply {
             for (i in 0 .. 30) {
                 when (i % 3) {
-                    0 -> this.add(Product(productType + i, "상품이름${productType + i}", productTypes[i % 3], (productType + i) * 1_000,"drink.png", false, false))
-                    1 -> this.add(Product(productType + i, "상품이름${productType + i}", productTypes[i % 3], (productType + i) * 1_000,"drink.png", true, false))
-                    2 -> this.add(Product(productType + i, "상품이름${productType + i}", productTypes[i % 3], (productType + i) * 1_000,"drink.png", false, true))
+                    0 -> this.add(Product(productType + i, "상품이름${productType + i}", productTypes[i % 3], (productType + i) * 1_000,"ICED 카페 모카.png", false, false))
+                    1 -> this.add(Product(productType + i, "상품이름${productType + i}", productTypes[i % 3], (productType + i) * 1_000,"ICED 카페 아메리카노.png", true, false))
+                    2 -> this.add(Product(productType + i, "상품이름${productType + i}", productTypes[i % 3], (productType + i) * 1_000,"(EX) ICED 버블 흑당 콜드브루.png", false, true))
                 }
 
             }
         }
-
+        
         binding.apply {
             productAdapter = ProductAdapter(requireContext(), productList)
-            lvProducts.adapter = productAdapter
+            productAdapter.setOnItemClickListener(object : ProductAdapter.OnItemClickListener {
+                override fun onItemClick(v: View, pos: Int) {
+                    Log.d(TAG, "onItemClick: ${pos}")
+                    Toast.makeText(requireContext(), "${productList[pos].name}", Toast.LENGTH_SHORT).show()
+//                    v.product = productList[pos]
+//                    mainActivity.openFragment(3, "productId", productId)
+                }
+            })
+
+            recyclerViewMenu.apply {
+                layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+                adapter = productAdapter
+//                adapter!!.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            }
         }
-
-
-
-
-
     }
 }
--- app/src/main/java/com/ssafy/cobaltcoffee/home/order/ProductAdapter.kt
+++ app/src/main/java/com/ssafy/cobaltcoffee/home/order/ProductAdapter.kt
@@ -1,38 +1,71 @@
 package com.ssafy.cobaltcoffee.home.order
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.ssafy.cobaltcoffee.R
+import com.ssafy.cobaltcoffee.config.ApplicationClass
+import com.ssafy.cobaltcoffee.databinding.ProductLvItemBinding
 import com.ssafy.cobaltcoffee.dto.Product
+import com.ssafy.cobaltcoffee.util.CommonUtils
 
-class ProductAdapter (val context: Context, val products: ArrayList<Product>): BaseAdapter() {
-    override fun getCount(): Int = products.size
-
-    override fun getItem(position: Int): Product = products[position]
-
-    override fun getItemId(position: Int): Long = position.toLong()
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var view: View = LayoutInflater.from(parent?.context).inflate(R.layout.product_lv_item, parent, false)
-
-        val item: Product = products[position]
-
-        val tvProductName: TextView = view.findViewById<TextView>(R.id.product_name)
-        val tvProductPrice: TextView = view.findViewById<TextView>(R.id.product_price)
+private const val TAG = "ProductAdapter_코발트"
+class ProductAdapter (val context: Context, val products: List<Product>)
+    : RecyclerView.Adapter<ProductAdapter.ListHolder>() {
+    inner class ListHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val productImage: ImageView = view.findViewById<ImageView>(R.id.product_image)
+        val productName: TextView = view.findViewById<TextView>(R.id.product_name)
+        val productPrice: TextView = view.findViewById<TextView>(R.id.product_price)
         val tagNew: ImageView = view.findViewById<ImageView>(R.id.tag_new)
         val tagBest: ImageView = view.findViewById<ImageView>(R.id.tag_best)
 
-        tvProductName.text = item.name
-        tvProductPrice.text = item.price.toString() + "원"
+        fun bindInfo(product: Product){
+            Glide.with(context)
+                .load("${ApplicationClass.MENU_IMGS_URL}${product.image}")
+                .transform(CenterCrop())
+                .into(productImage)
+            productName.text = product.name
+            productPrice.text = CommonUtils.makeComma(product.price)
 
-        if (!item.isNew) tagNew.visibility = View.GONE
-        if (!item.isBest) tagBest.visibility = View.GONE
+            if (!product.isNew) tagNew.visibility = View.GONE
+            if (!product.isBest) tagBest.visibility = View.GONE
 
-        return view
+            val pos = adapterPosition
+            if (pos != RecyclerView.NO_POSITION) {
+                itemView.setOnClickListener {
+                    listener?.onItemClick(itemView, pos)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListHolder {
+        val binding = ProductLvItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: ListHolder, position: Int) {
+        holder.bindInfo(products[position])
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    interface OnItemClickListener{
+        fun onItemClick(v:View, pos: Int)
+    }
+
+    private var listener : OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener : OnItemClickListener) {
+        this.listener = listener
     }
 }
