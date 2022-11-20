1ba3f082c7f35b2f7fb12e78a5aee60634657f39
su6378 <josooyeon11@naver.com>
2022-11-20 18:14:16 +0900

    HomeFragment UI 추가 및 수정

--- app/src/main/java/com/ssafy/cobaltcoffee/home/HomeFragment.kt
+++ app/src/main/java/com/ssafy/cobaltcoffee/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.ssafy.cobaltcoffee.R
 import com.ssafy.cobaltcoffee.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -20,13 +22,22 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-        init()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+
+        binding.homeBanner.setOnClickListener {
+
+        }
     }
 
     private fun init() {
 
-
+        Glide.with(requireContext()).load(R.drawable.background_home).into(binding.homeImg)
+        Glide.with(requireContext()).load(R.drawable.banner).into(binding.homeBanner)
 
 
 
