c8ddc98b112f87d2b7a8801d298df8b6b56f0c64
rigizer <rigizer@gmail.com>
2022-11-22 16:51:29 +0900

    SpringBoot: CouponServiceImpl @Service 애노테이션 추가

--- mobile_through_project_final/src/main/java/com/ssafy/cafe/model/service/CouponServiceImpl.java
+++ mobile_through_project_final/src/main/java/com/ssafy/cafe/model/service/CouponServiceImpl.java
@@ -3,11 +3,13 @@ package com.ssafy.cafe.model.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.cafe.model.dao.CouponDao;
 import com.ssafy.cafe.model.dto.Coupon;
 
+@Service
 public class CouponServiceImpl implements CouponService {
     
     @Autowired
