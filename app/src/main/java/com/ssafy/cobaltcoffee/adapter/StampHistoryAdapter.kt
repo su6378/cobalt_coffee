d7ce43421df1a742a2695afdb7c6612b020a80c4
su6378 <josooyeon11@naver.com>
2022-11-22 17:46:11 +0900

    스탬프 적립 내역 추가

--- app/src/main/java/com/ssafy/cobaltcoffee/adapter/StampHistoryAdapter.kt
+++ app/src/main/java/com/ssafy/cobaltcoffee/adapter/StampHistoryAdapter.kt
@@ -1,5 +1,6 @@
 package com.ssafy.cobaltcoffee.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,36 +10,39 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ssafy.cobaltcoffee.R
 import com.ssafy.cobaltcoffee.config.ApplicationClass
+import com.ssafy.cobaltcoffee.dto.LatestOrder
 import com.ssafy.cobaltcoffee.dto.Product
 import com.ssafy.cobaltcoffee.response.OrderDetailResponse
 import com.ssafy.cobaltcoffee.util.CommonUtils
 
 private const val TAG = "StampHistoryAdapter_코발트"
-class StampHistoryAdapter(var stampList:List<OrderDetailResponse>) :RecyclerView.Adapter<StampHistoryAdapter.StampHistoryHolder>(){
+class StampHistoryAdapter(var orderList:List<LatestOrder>) :RecyclerView.Adapter<StampHistoryAdapter.StampHistoryHolder>(){
 
     inner class StampHistoryHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val stampCount = itemView.findViewById<TextView>(R.id.stampHistory_stampCount)
         val stampDate = itemView.findViewById<TextView>(R.id.stampHistory_date)
-
-        fun bindInfo(stamp : OrderDetailResponse){
-            stampCount.text = "+${stamp.stampCount}"
-            stampDate.text = CommonUtils.getFormattedString(stamp.orderDate)
+        
+        fun bindInfo(order : LatestOrder){
+            Log.d(TAG, "orderList: $orderList")
+            stampCount.text = "+${order.productName}"
+            stampDate.text = CommonUtils.getFormattedString(order.orderDate)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StampHistoryHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_stamp_card, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_stamp_history, parent, false)
         return StampHistoryHolder(view)
     }
 
     override fun onBindViewHolder(holder: StampHistoryHolder, position: Int) {
         holder.apply{
-            bindInfo(stampList[position])
+            bindInfo(orderList[position])
         }
     }
 
     override fun getItemCount(): Int {
-        return stampList.size
+        Log.d(TAG, "getItemCount: ${orderList.size}")
+        return orderList.size
     }
 
     //클릭 인터페이스 정의 사용하는 곳에서 만들어준다.
--- app/src/main/java/com/ssafy/cobaltcoffee/stamp/StampHistoryFragment.kt
+++ app/src/main/java/com/ssafy/cobaltcoffee/stamp/StampHistoryFragment.kt
@@ -14,13 +14,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.ssafy.cobaltcoffee.R
+import com.ssafy.cobaltcoffee.adapter.CurrentOrderAdapter
 import com.ssafy.cobaltcoffee.adapter.OrderDetailAdapter
 import com.ssafy.cobaltcoffee.adapter.StampCardAdapter
 import com.ssafy.cobaltcoffee.adapter.StampHistoryAdapter
 import com.ssafy.cobaltcoffee.databinding.FragmentStampCardBinding
+import com.ssafy.cobaltcoffee.databinding.FragmentStampHistoryBinding
+import com.ssafy.cobaltcoffee.dto.LatestOrder
 import com.ssafy.cobaltcoffee.dto.Order
 import com.ssafy.cobaltcoffee.dto.User
 import com.ssafy.cobaltcoffee.dto.UserLevel
+import com.ssafy.cobaltcoffee.home.OrderFragment
 import com.ssafy.cobaltcoffee.repository.OrderRepository
 import com.ssafy.cobaltcoffee.repository.UserRepository
 import com.ssafy.cobaltcoffee.response.OrderDetailResponse
@@ -33,11 +37,11 @@ import kotlin.collections.HashMap
 
 private const val TAG = "StampHistoryFragment_코발트"
 class StampHistoryFragment : Fragment() {
-    private lateinit var binding: FragmentStampCardBinding
+    private lateinit var binding: FragmentStampHistoryBinding
     private lateinit var stampActivity: StampActivity
 
     private lateinit var stampHistoryAdapter: StampHistoryAdapter
-    private lateinit var orderDetailList : ArrayList<Pair<Int, Date>>
+    private var orderList: MutableList<LatestOrder> = mutableListOf()
 
     private val userViewModel: UserViewModel by activityViewModels()
 
@@ -50,17 +54,53 @@ class StampHistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentStampCardBinding.inflate(inflater,container,false)
+        binding = FragmentStampHistoryBinding.inflate(inflater,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         init()
     }
 
     private fun init(){
+
+        getAllOrder()
+
+        binding.apply {
+            stampHistoryAdapter = StampHistoryAdapter(orderList)
+
+            stampHistoryRv.apply {
+                layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+                adapter = stampHistoryAdapter
+                //원래의 목록위치로 돌아오게함
+                adapter!!.stateRestorationPolicy =
+                    RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            }
+        }
+    }
+
+    //전체 주문내역 가져오기 : Retrofit
+    private fun getAllOrder() {
+        OrderRepository.get()
+            .getAllOrder(userViewModel.currentUser.id, AllOrderListCallback())
+    }
+
+    inner class AllOrderListCallback : RetrofitCallback<List<LatestOrder>> {
+        override fun onSuccess(code: Int, result: List<LatestOrder>) {
+            orderList = result as MutableList<LatestOrder>
+            stampHistoryAdapter.orderList = orderList
+            stampHistoryAdapter.notifyDataSetChanged()
+        }
+
+        override fun onError(t: Throwable) {
+            Log.d(TAG, t.message ?: "전체 주문 내역 불러오는 중 통신오류")
+        }
+
+        override fun onFailure(code: Int) {
+            Log.d(TAG, "onResponse: Error Code $code")
+        }
     }
 
 }
